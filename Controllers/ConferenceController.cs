a6034b9844b43ea398031a4883338670ac2ad5d7
German Dario <germangp088@gmail.com>
2020-06-27 22:21:13 -0300

    Upload project

---
+++ Controllers/ConferenceController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Services;
+
+namespace Conferences.Controllers
+{
+    public class ConferenceController: Controller
+    {
+        public ConferenceController(IConferenceService conferenceService)
+        {
+
+        }
+    }
+}
